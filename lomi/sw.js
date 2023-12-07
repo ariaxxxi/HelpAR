1e56a6a85ef486717effcbe60fe794d653016126
xxxi <87568028+ariaxxxi@users.noreply.github.com>
2023-12-07 13:59:33 -0500

    update folder structure

---
+++ lomi/sw.js
@@ -0,0 +1,27 @@
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open('helpar-soom').then(function(cache) {
+      return cache.addAll([
+        '/',
+        './js/jq.min.js',
+        './js/bootstrap.bundle.min.js.map',
+        './js/bootstrap.bundle.min.js',
+        
+        './css/work.css',
+        './css/us.css',
+        './css/swiper.css',
+        './css/index.css',
+        './css/bootstrap.min.css.map',
+        './css/bootstrap.min.css'
+      ]);
+    })
+  );
+  });
+
+self.addEventListener('fetch', function(event) {  
+event.respondWith(
+  caches.match(event.request).then(function(response) {
+  return response || fetch(event.request);
+  })
+);
+});
