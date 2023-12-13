23a1738884f5262ece45839590d1a49ee45bee8d
xxxi <87568028+ariaxxxi@users.noreply.github.com>
2023-12-13 12:51:39 -0500

    add frolic

--- js/sw.js
+++ js/sw.js
@@ -3,25 +3,29 @@ self.addEventListener('install', function(e) {
     caches.open('helpar').then(function(cache) {
       return cache.addAll([
         '/',
-        './js/jq.min.js',
-        './js/bootstrap.bundle.min.js.map',
-        './js/bootstrap.bundle.min.js',
+        './jq.min.js',
+        './bootstrap.bundle.min.js.map',
+        './bootstrap.bundle.min.js',
         
-        './css/work.css',
-        './css/us.css',
-        './css/swiper.css',
-        './css/index.css',
-        './css/bootstrap.min.css.map',
-        './css/bootstrap.min.css'
-      ]);
+        '../css/work.css',
+        '../css/us.css',
+        '../css/swiper.css',
+        '../css/index.css',
+        '../css/bootstrap.min.css.map',
+        '../css/bootstrap.min.css'
+      ]).catch(function(error) {
+        console.error('Cache addAll failed:', error);
+      });
     })
   );
-  });
+});
 
 self.addEventListener('fetch', function(event) {  
-event.respondWith(
-  caches.match(event.request).then(function(response) {
-  return response || fetch(event.request);
-  })
-);
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request).catch(function(error) {
+        console.error('Fetch failed:', error);
+      });
+    })
+  );
 });
